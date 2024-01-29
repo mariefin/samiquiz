4f3882f3859affd7202a9445373a4dcb2c9275f0
mariefin <maikki_maa_93@hotmail.com>
2024-01-29 21:20:16 +0200

    Vearba version.

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,46 +1,69 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../index.css';
 import Col from 'react-bootstrap/Col';
+import axios from "axios";
 
 
 function Test() {
+
+    const randomSatneLuohka = () => {
+        const satneLuohkat = ["ol1pres",
+        "ol2pres",
+        "ol3pres",
+        "gl1pres",
+        "gl2pres",
+        "gl3pres",
+        "ml1pres",
+        "ml2pres",
+        "ml3pres",
+        "ol1pret",
+        "ol2pret",
+        "ol3pret",
+        "gl1pret",
+        "gl2pret",
+        "gl3pret",
+        "ml1pret",
+        "ml2pret",
+        "ml3pret"]
+
+        const randomNumber = Math.floor(Math.random() * satneLuohkat.length);
+        return satneLuohkat[randomNumber];
+    }
+
     const [currentQuestion, setCurrentQuestion] = useState<number>(0);
     const [score, setScore] = useState<number>(0);
     const [answers, setAnswers] = useState<string[]>([]);
     const [showScore, setShowScore] = useState(false);
+    const [satnit, setSatnit] = useState([{"vearba": "leat"}]);
+    const [currentSatni, setCurrentSatni] = useState<number>(0);
+    const maxQuestions : number = 10;
+    const [satneLuohka, setSatneLuohka] = useState<string>(randomSatneLuohka);
 
-    const questions = [
-        {
-          question: 'Who is the CEO of Tesla?',
-          type: 'input',
-          answer: 'Elon Musk',
-        },
-        {
-          question: 'Share your feedback about this quiz:',
-          type: 'input',
-          answer: 'Nice',
-        }
-        // Add more questions here
-      ];
+    useEffect(() => {
+        axios.get('/satnis/vearba.json').then(response => {
+            setSatnit(response.data);
+            setCurrentSatni(Math.floor(Math.random() * 3));
+        });
+    }, []);
+
+    // setCurrentSatni(Math.floor(Math.random() * satnis.length));
+    
     const handleAnswerSelection = (questionIndex : number, selectedAnswer : string) => {
-        console.log(questionIndex, selectedAnswer);
         const updatedAnswers : string[] = [...answers];
         updatedAnswers[questionIndex] = selectedAnswer;
         setAnswers(updatedAnswers);
     };
 
+
     const handleNextQuestion = () => {
-        console.log(answers[currentQuestion], questions[currentQuestion].answer);
-        if (
-          answers[currentQuestion] === questions[currentQuestion].answer ||
-          JSON.stringify(answers[currentQuestion]) ===
-            JSON.stringify(questions[currentQuestion].answer)
-        ) {
-            console.log('what');
+        if (answers[currentQuestion] === satnit[currentSatni][satneLuohka] ) {
           setScore(score + 1);
         }
-        if (currentQuestion + 1 < questions.length) {
-          setCurrentQuestion(currentQuestion + 1);
+        if (currentQuestion + 1 < maxQuestions) {
+            setCurrentQuestion(currentQuestion + 1);
+            setCurrentSatni(Math.floor(Math.random() * satnit.length));
+            console.log(Math.floor(Math.random() * satnit.length), satnit);
+            setSatneLuohka(randomSatneLuohka);
         } else {
           setShowScore(true);
         }
@@ -54,20 +77,24 @@ function Test() {
         <div>
             <h2>Quiz Complete!</h2>
             <h3>Your Score: {score}</h3>
+            <p>{answers}</p>
         </div>
             ) : (
             <div>
                 <h2>Question {currentQuestion + 1}</h2>
-                <h3>{questions[currentQuestion].question}</h3>
-                {questions[currentQuestion].type === 'input' && (
+                <h3>{satnit[currentSatni]['vearba']}</h3>
+                <p>{satneLuohka}</p>
+                <div>
                 <input
                     type="text"
                     onChange={(e) =>
                     handleAnswerSelection(currentQuestion, e.target.value)
                     }
+                    className="custom-input"
                 />
-                )}
-                <button onClick={handleNextQuestion} className="btn btn-primary">Next Question</button>
+                </div>
+                
+                <button onClick={handleNextQuestion} className="btn btn-primary mt-4">Next Question</button>
             </div>
             )}
         </Col>
